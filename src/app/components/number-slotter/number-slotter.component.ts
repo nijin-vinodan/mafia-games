386fd919d20a816118d527d0cb1aa76b4c486d90
Nijin <nijin.vinodan@siriuscom.com>
2019-10-31 10:35:25 +0530

    Number Slotter Component added

---
+++ src/app/components/number-slotter/number-slotter.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-number-slotter',
+  templateUrl: './number-slotter.component.html',
+  styleUrls: ['./number-slotter.component.scss'],
+})
+export class NumberSlotterComponent implements OnInit {
+
+  count : number = 0;
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+  /**
+   * Increment Counter Value
+   */
+  incrementCount(){
+    this.count += 1;
+  }
+
+  /**
+   * Decrement Counter Value
+   */
+  decrementCount(){
+    if(this.count - 1 >= 0){
+      this.count -= 1;
+    } 
+  }
+
+}
